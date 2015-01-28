2db3257e93d63a2177799cd8515c5293cd22c297
Fabrice Brito <fabrice.brito@terradue.com>
2015-01-28 16:32:56 +0100

    Update run.sh

--- src/main/app-resources/slave_slc/run.sh
+++ src/main/app-resources/slave_slc/run.sh
@@ -5,25 +5,43 @@ mode=$1
 [ "${mode}" != "test" ] && source ${ciop_job_include}
 
 # define the exit codes
+# define the exit codes
 SUCCESS=0
+ERR_ORBIT_FLAG=5
+ERR_MASTER_SLC=7
+ERR_SLC_AUX=9
+ERR_SLAVE=9
+ERR_SLAVE_SENSING_DATE=11
+ERR_MISSION_SLAVE=13
+ERR_AUX=15
+ERR_SLC=17
+ERR_SLC_TAR=19
+ERR_SLC_PUBLISH=21
 
 # add a trap to exit gracefully
 cleanExit () {
   local retval=$?
   local msg
-
   msg=""
+
   case "${retval}" in
     ${SUCCESS}) msg="Processing successfully concluded";;
+    ${ERR_ORBIT_FLAG}) msg="Failed to determine which orbit files to use";;
+    ${ERR_MASTER_SLC}) msg="Couldn't retrieve master slc";;
+    ${ERR_SLC_AUX}) msg="Couldn't retrieve master slc ROI_PAC aux files";;
+    ${ERR_SLAVE}) msg="Couldn't retrieve slave";;
+    ${ERR_SLAVE_SENSING_DATE}) msg="Couldn't determine slave sensing day";;
+    ${ERR_MISSION_SLAVE}) msg="Couldn't determine slave mission";;
+    ${ERR_AUX}) msg="Couldn't retrieve slave aux files";;
+    ${ERR_SLC}) msg="Failed to process slc";;
+    ${ERR_SLC_TAR}) msg="Failed to create archive with slave slc";;
+    ${ERR_SLC_PUBLISH}) msg="Failed to publish archive slave slc";;
   esac
-   
   [ "${retval}" != "0" ] && ciop-log "ERROR" \
     "Error ${retval} - ${msg}, processing aborted" || ciop-log "INFO" "${msg}"
-
   [ -n "${TMPDIR}" ] && rm -rf ${TMPDIR}
   [ "${mode}" == "test" ] && return ${retval} || exit ${retval}
 }
-
 trap cleanExit EXIT
 
 main() {
@@ -43,46 +61,53 @@ main() {
   
     [ ${first} == "TRUE"] && {
       ciop-copy -O ${SLC} ${master_slc_ref}
-      # TODO check copy
+      [ $? -ne 0 ] && return ${ERR_MASTER_SLC}
       ciop-copy -O ${SLC} ${txt_ref}
-      # TODO check copy 
+      [ $? -ne 0 ] && return ${ERR_MASTER_SLC}
       first=FALSE
     }
   
-  slave=$( ciop-copy -O $TMPDIR )
+    slave=$( ciop-copy -O $TMPDIR )
+    [ $? -ne 0 ] && return ${ERR_SLAVE}
   
-  sensing_date=$( get_sensing_date $slave )
-  [ $? -ne 0 ] && return ${ERR_SLAVE_SENSING_DATE}
+    sensing_date=$( get_sensing_date $slave )
+    [ $? -ne 0 ] && return ${ERR_SLAVE_SENSING_DATE}
   
-  mission=$( get_mission ${slave} | tr "A-Z" "a-z" )
-  [ $? -ne 0 ] && return ${ERR_MISSION_SLAVE}
-  [ ${mission} == "asar" ] && flag="envi"
+    mission=$( get_mission ${slave} | tr "A-Z" "a-z" )
+    [ $? -ne 0 ] && return ${ERR_MISSION_SLAVE}
+    [ ${mission} == "asar" ] && flag="envi"
   
-  # TODO manage ERS and ALOS
-  # [ ${mission} == "alos" ] && flag="alos"
-  # [ ${mission} == "ers" ] && flag="ers"
-  # [ ${mission} == "ers_envi" ] && flag="ers_envi"
+    # TODO manage ERS and ALOS
+    # [ ${mission} == "alos" ] && flag="alos"
+    # [ ${mission} == "ers" ] && flag="ers"
+    # [ ${mission} == "ers_envi" ] && flag="ers_envi"
   
-  slave_folder=${SLC}/${sensing_date}
-  mkdir -p ${slave_folder}
+    slave_folder=${SLC}/${sensing_date}
+    mkdir -p ${slave_folder}
   
-  get_aux ${mission} ${sensing_date} ${orbits} 
-  [ $? -ne 0 ] && return ${ERR_AUX}
+    get_aux ${mission} ${sensing_date} ${orbits} 
+    [ $? -ne 0 ] && return ${ERR_AUX}
 
-  cd ${_folder}
-  ln -s ${slave}
-  slc_bin="step_slc_${flag}$( [ ${orbits} == "VOR" ] && [ ${mission} == "asar" ] && echo "_vor")"
-  ciop-log "INFO" "Run $slc_bin for ${sensing_date}"
+    cd ${slave_folder}
+    ln -s ${slave}
+    slc_bin="step_slc_${flag}$( [ ${orbits} == "VOR" ] && [ ${mission} == "asar" ] && echo "_vor")"
+    ciop-log "INFO" "Run $slc_bin for ${sensing_date}"
   
-  ${slc_bin}
-  [ $? -ne 0 ] && return ${ERR_SLC}
+    ${slc_bin}
+    [ $? -ne 0 ] && return ${ERR_SLC}
   
-  cd ${SLC}
-  tar cvfz ${sensing_date}.tgz ${sensing_date}
+    cd ${SLC}
+    tar cvfz ${sensing_date}.tgz ${sensing_date}
+    [ $? -ne 0 ] && return ${ERR_SLC_TAR}
   
-  ciop-publish ${sensing_date}.tgz
+    slave_slc_ref=$( ciop-publish ${sensing_date}.tgz )
+    [ $? -ne 0 ] && return ${ERR_SLC_PUBLISH}
   
-  rm -fr ${SLC}/${sensing_date}
+    # publish the references for the next job
+    echo "${master_slc_ref} ${txt_ref} ${slave_slc_ref}" | ciop-publish -s
+    rm -fr ${SLC}/${sensing_date}
+
+  done
 }
 
 cat | main 
