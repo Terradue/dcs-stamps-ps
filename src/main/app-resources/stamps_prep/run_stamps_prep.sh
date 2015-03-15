dc0b044e1d06b18986993128ee65c28dcdc6bbc2
BuddyVolly <andreasvollrath@gmx.de>
2015-03-15 20:49:57 +0100

    adding node 5

--- src/main/app-resources/stamps_prep/run_stamps_prep.sh
+++ src/main/app-resources/stamps_prep/run_stamps_prep.sh
@@ -22,7 +22,15 @@ set_env
 SUCCESS=0
 ERR_MASTER_RETRIEVE=7
 ERR_DEM_RETRIEVE=9
-ERR_SLC_RETRIEVE=11
+ERR_INSAR_SLAVE_RETRIEVE=11
+ERR_STEP_GEO=13
+ERR_MT_PREP=15
+ERR_STAMPS_1=17
+ERR_INSAR_TAR=19
+ERR_INSAR_PUBLISH=21
+ERR_PATCH_TAR=23
+ERR_PATCH_PUBLISH=25
+ERR_FINAL_PUBLISH=27
 
 # add a trap to exit gracefully
 cleanExit() {
@@ -33,11 +41,14 @@ case "${retval}" in
 ${SUCCESS}) msg="Processing successfully concluded";;
 ${ERR_MASTER_RETRIEVE}) msg="Failed to retrieve Master folder";;
 ${ERR_DEM_RETRIEVE}) msg="Failed to retrieve DEM folder";;
-${ERR_SLC_RETRIEVE) msg="Failed to retrieve SLC folder";;
-
-
-${ERR_INSAR_SLAVES_TAR}) msg="Failed to tar Insar Slave folder";;
-${ERR_INSAR_SLAVES_PUBLISH}) msg="Failed to publish Insar Slave folder";;
+${ERR_INSAR_SLAVE_RETRIEVE}) msg="Failed to tar Insar Slave folder";;
+${ERR_STEP_GEO}) msg="Failed to geoference the image stack";; 
+${ERR_MT_PREP}) msg="Failed to run mt_prep routine";;
+${ERR_STAMPS_1}) msg="Failed to process step 1 of StaMPS";;
+${ERR_INSAR_TAR}) msg="Failed to tar Insar Slave folder";;
+${ERR_INSAR_PUBLISH}) msg="Failed to publish Insar Slave folder";;
+${ERR_PATCH_TAR}) msg="Failed to tar Insar Slave folder";;
+${ERR_PATCH_PUBLISH}) msg="Failed to publish Insar Slave folder";;
 ${ERR_FINAL_PUBLISH}) msg="Failed to publish all output together";;
 esac
 [ "${retval}" != "0" ] && ciop-log "ERROR" \
@@ -80,11 +91,13 @@ while read line; do
 	[ $? -ne 0 ] && return ${ERR_DEM_RETRIEVE}
 
 #	fi
-
+	}
+	
 	ciop-log "INFO" "Retrieving SLC folder"
 	ciop-copy -O ${PROCESS}/INSAR_${master_date} ${insar_slaves}
-	[ $? -ne 0 ] && return ${ERR_SLC_RETRIEVE}
+	[ $? -ne 0 ] && return ${ERR_INSAR_SLAVE_RETRIEVE}
 
+[ ${first} == "TRUE" ] && {
 	ciop-log "INFO" "Georeferencing image stack"
  	first_folder=`ls -d -1 2*/`
 	cd ${PROCESS}/INSAR_${master_date}/$first_folder
@@ -100,12 +113,46 @@ cd ${PROCESS}/INSAR_${master_date}/
 mt_prep 0.42 4 5 50 200
 [ $? -ne 0 ] && return ${ERR_MT_PREP}
 
+# Check for size of pscands.1.da to see if enough PS are contained
+rm patch.list
+ls -1 -s */pscands.1.da > patch_size.txt
+while read line; do
+ 	PATCH_SIZE=`echo $line | awk $'{print $ 1}'`
+	if [[ "${PATCH_SIZE}" -gt "100" ]] ; then
+		VALID_PATCHES=`echo $line | awk $'{print $2}' | awk -F '/' $'{print $1}'`
+		echo $VALID_PATCHES >> patch.list
+	fi
+done < patch_size.txt
 
-tar cvfz INSAR_${master_date}.tgz *.txt *.out *.res *.in 
+ciop-log "INFO" "Running Stamps step 1"
+/opt/StaMPS_v3.3b1/matlab/stamps MCR 1 1
+[ $? -ne 0 ] && return ${ERR_STAMPS_1}
 
-ls -d -1 P*
+ciop-log "INFO" "creating tar for InSAR Master folder"
+tar cvfz INSAR_${master_date}.tgz *.txt *.out *.res *.in *.m *.mat 
+[ $? -ne 0 ] && return ${ERR_INSAR_TAR}
 
-stamps(1,2)
+ciop-log "INFO" "publishing InSAR Master folder"
+insar_master="$( ciop-publish -a ${PROCESS}/INSAR_${master_date}.tgz )"
+[ $? -ne 0 ] && return ${ERR_INSAR_PUBLISH}
 
-#stamps(1,1)
+while read line; do
+
+	ciop-log "INFO" "Tar $line"
+	tar cvfz $line.tgz $line
+	[ $? -ne 0 ] && return ${ERR_PATCH_TAR}
+	
+	ciop-log "INFO" "publishing $line"
+	patches="$( ciop-publish -a ${PROCESS}/INSAR_${master_date}/$line.tgz )"
+	[ $? -ne 0 ] && return ${ERR_PATCH_PUBLISH}
+
+	ciop-log "INFO" "publishing the final output"
+	echo "${insar_master},${patches}" | ciop-publish -s	
+	[ $? -ne 0 ] && return ${ERR_FINAL_PUBLISH}
+}
+
+done < patch.list
+
+cat | main
+exit ${SUCCESS}
 
