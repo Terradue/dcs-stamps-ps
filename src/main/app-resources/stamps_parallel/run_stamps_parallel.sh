053f080e64a317ac97e85f6eaf6f6e7a3257c3d8
BuddyVolly <andreasvollrath@gmx.de>
2015-03-22 19:17:06 +0100

    testing stamps final node

--- src/main/app-resources/stamps_parallel/run_stamps_parallel.sh
+++ src/main/app-resources/stamps_parallel/run_stamps_parallel.sh
@@ -23,15 +23,14 @@ MCR="/usr/local/MATLAB/MATLAB_Compiler_Runtime/v717"
 # define the exit codes
 SUCCESS=0
 ERR_MASTER_RETRIEVE=7
-ERR_DEM_RETRIEVE=9
-ERR_INSAR_SLAVE_RETRIEVE=11
-ERR_STEP_GEO=13
-ERR_MT_PREP=15
-ERR_STAMPS_1=17
-ERR_INSAR_TAR=19
-ERR_INSAR_PUBLISH=21
+ERR_PATCH_RETRIEVE=7
+ERR_STAMPS_2=17
+ERR_STAMPS_3=17
+ERR_STAMPS_4=17
 ERR_PATCH_TAR=23
 ERR_PATCH_PUBLISH=25
+ERR_INSAR_TAR=19
+ERR_INSAR_PUBLISH=21
 ERR_FINAL_PUBLISH=27
 
 # add a trap to exit gracefully
@@ -42,15 +41,14 @@ msg=""
 case "${retval}" in
 ${SUCCESS}) msg="Processing successfully concluded";;
 ${ERR_MASTER_RETRIEVE}) msg="Failed to retrieve Master folder";;
-${ERR_DEM_RETRIEVE}) msg="Failed to retrieve DEM folder";;
-${ERR_INSAR_SLAVE_RETRIEVE}) msg="Failed to tar Insar Slave folder";;
-${ERR_STEP_GEO}) msg="Failed to geoference the image stack";; 
-${ERR_MT_PREP}) msg="Failed to run mt_prep routine";;
-${ERR_STAMPS_1}) msg="Failed to process step 1 of StaMPS";;
-${ERR_INSAR_TAR}) msg="Failed to tar Insar Slave folder";;
-${ERR_INSAR_PUBLISH}) msg="Failed to publish Insar Slave folder";;
+${ERR_PATCH_RETRIEVE}) msg="Failed to retrieve the PATCH folder";;
+${ERR_STAMPS_2}) msg="Failed to process step 1 of StaMPS";;
+${ERR_STAMPS_3}) msg="Failed to process step 1 of StaMPS";;
+${ERR_STAMPS_4}) msg="Failed to process step 1 of StaMPS";;
 ${ERR_PATCH_TAR}) msg="Failed to tar Insar Slave folder";;
 ${ERR_PATCH_PUBLISH}) msg="Failed to publish Insar Slave folder";;
+${ERR_INSAR_TAR}) msg="Failed to tar Insar Slave folder";;
+${ERR_INSAR_PUBLISH}) msg="Failed to publish Insar Slave folder";;
 ${ERR_FINAL_PUBLISH}) msg="Failed to publish all output together";;
 esac
 [ "${retval}" != "0" ] && ciop-log "ERROR" \
@@ -113,15 +111,33 @@ while read line; do
 	#fi
 	#rm tmp
 
-	cd ../../
+	cd ../
 
+	ciop-log "INFO" "Tar $patch"
+	tar cvfz $patch.tgz $patch
+	[ $? -ne 0 ] && return ${ERR_PATCH_TAR}
+	
+	ciop-log "INFO" "publishing $line"
+	patches="$( ciop-publish -a ${PROCESS}/INSAR_${master_date}/$patch.tgz )"
+	[ $? -ne 0 ] && return ${ERR_PATCH_PUBLISH}
+
+	rm -rf $patch
+
+	cd ../
 	ciop-log "INFO" "creating tar for InSAR Master folder"
 	tar cvfz INSAR_${master_date}.tgz  INSAR_${master_date}
 	[ $? -ne 0 ] && return ${ERR_INSAR_TAR}
 
 	ciop-log "INFO" "publishing InSAR Master folder"
 	ciop-publish ${PROCESS}/INSAR_${master_date}.tgz
+	insar_master="$( ciop-publish -a ${PROCESS}/INSAR_${master_date}.tgz )"
 	[ $? -ne 0 ] && return ${ERR_INSAR_PUBLISH}
+
+
+	ciop-log "INFO" "publishing the final output"
+	echo "${insar_master},${patches}" | ciop-publish -s	
+	[ $? -ne 0 ] && return ${ERR_FINAL_PUBLISH}
+
 done
 
 }
