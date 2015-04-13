81eb108abcd22abedb4d1992d97c169fd7a3f595
BuddyVolly <andreasvollrath@gmx.de>
2015-04-13 11:31:40 +0200

    finalizing workflow

--- src/main/app-resources/stamps_prep/run_stamps_prep.sh
+++ src/main/app-resources/stamps_prep/run_stamps_prep.sh
@@ -130,8 +130,8 @@ grep SAM_IN_UL $INSARDIR/timing.dorisin | gawk '{if ($1=="SAM_IN_UL") print $2}'
 grep SAM_IN_DELTA $INSARDIR/timing.dorisin | gawk '{if ($1=="SAM_IN_DELTA") print $2}' >> $INSARDIR/demparms.in 
 grep SAM_IN_FORMAT $INSARDIR/timing.dorisin | gawk '{if ($1=="SAM_IN_FORMAT") print $2}' >> $INSARDIR/demparms.in 
 
-mt_prep 0.42 5 6 50 200
-#mt_prep 0.42 1 2 50 200
+#mt_prep 0.42 5 6 50 200
+mt_prep 0.42 1 2 50 200
 [ $? -ne 0 ] && return ${ERR_MT_PREP}
 
 # Check for size of pscands.1.da to see if enough PS are contained
